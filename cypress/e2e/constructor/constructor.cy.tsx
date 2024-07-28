071d63bbd8472339a375c66ef3ae80d933b21187
Irina Syrcova <irinasyrcova223@gmail.com>
2024-07-28 15:33:12 +0300

    исправления

--- cypress/e2e/constructor/constructor.cy.tsx
+++ cypress/e2e/constructor/constructor.cy.tsx
@@ -1,31 +1,32 @@
+const testUrl = 'http://localhost:4000';
+
 describe('Тест конструктора', () => {
     beforeEach(() => {
         cy.viewport(1300, 800);
         cy.intercept('GET', 'api/auth/user', {fixture: 'user.json'})
         cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients.json'});
-        cy.visit('http://localhost:4000');
+        cy.visit(testUrl);
     })
     it('Добавление ингредиентов',() => {
-        cy.get('[data-cy="constructor"]')
-            .should('have.length', 0)
+        const constructor = cy.get('[data-cy="constructor"]')
+        constructor.should('have.length', 0)
         cy.get('[data-cy="main"]').contains('Добавить').click()
-        cy.get('[data-cy="constructor"]')
-            .should('have.length', 1)
+        constructor.should('have.length', 1)
     })
     it('Открытие и закрытие по клику на крестик модального окна ингредиента', () => {
         cy.get('[data-cy="bun"]').click()
-        cy.get('[data-cy="modal"]')
+        const modal = cy.get('[data-cy="modal"]')
         cy.get('[data-cy="closeBtn"]').click()
-        cy.get('[data-cy="modal"]').should('not.exist')
+        modal.should('not.exist')
     })
     it('Создание заказа', () => {
+        const constructor = cy.get('[data-cy="constructor"]')
         cy.intercept('POST', 'api/orders', {fixture: 'order.json'})
         cy.get('[data-cy="bun"]').contains('Добавить').click()
         cy.get('.button').click()
-        cy.get('[data-cy="modal"]').contains('1')
+        const modal = cy.get('[data-cy="modal"]')
         cy.get('[data-cy="closeBtn"]').click()
-        cy.get('[data-cy="modal"]').should('not.exist')
-        cy.get('[data-cy="constructor"]')
-            .should('not.have.length')
+        modal.should('not.exist')
+        constructor.should('not.have.length')
     })
 })
